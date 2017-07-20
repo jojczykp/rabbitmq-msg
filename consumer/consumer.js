6de73123c9ce6e6b6f5483f440bd1a4388bf19cb
jojczykp <pawel.jojczyk@gmail.com>
2017-07-20 01:13:59 +0100

    add exchange

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -1,21 +1,31 @@
+#!/usr/bin/env node
+
 'use strict;'
 
+var amqp = require('amqplib/callback_api');
+
 var host = '192.168.99.100';
-var queueName = 'sample-queue';
+var exchangeName = 'sample-exchange';
 
-var open = require('amqplib').connect('amqp://' + host);
+var args = process.argv.slice(2);
+if (args.length == 0) {
+    console.log("Usage: consumer.js userId");
+    process.exit(1);
+}
+var userId = args[0];
 
-console.log('Listening to queue ' + queueName + ' on host ' + host);
+amqp.connect('amqp://' + host, function(err, conn) {
+    conn.createChannel(function(err, ch) {
+        ch.assertExchange(exchangeName, 'direct', {durable: false});
 
-open.then(function(conn) {
-  return conn.createChannel();
-}).then(function(ch) {
-  return ch.assertQueue(queueName, {durable: false}).then(function(ok) {
-    return ch.consume(queueName, function(msg) {
-      if (msg !== null) {
-        console.log('Received: ' + msg.content.toString());
-        ch.ack(msg);
-      }
+        ch.assertQueue('', {exclusive: true}, function(err, q) {
+            console.log('Waiting for messages to %s@%s/%s. To exit press CTRL+C', userId, exchangeName, host);
+
+            ch.bindQueue(q.queue, exchangeName, userId);
+
+            ch.consume(q.queue, function(msg) {
+                console.log("Received on %s@%s/%s: %s", msg.fields.routingKey, msg.fields.exchange, host, msg.content.toString());
+            }, {noAck: true});
+        });
     });
-  });
-}).catch(console.warn);
+});
--- src/main/java/pl/jojczykp/rabbitmq_msg/Producer.java
+++ src/main/java/pl/jojczykp/rabbitmq_msg/Producer.java
@@ -5,35 +5,53 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
 import java.util.concurrent.TimeoutException;
 
 public class Producer {
 
     private static final String HOST = "192.168.99.100";
-    private final static String QUEUE_NAME = "sample-queue";
+    private final static String EXCHANGE_NAME = "sample-exchange";
 
     public static void main(String[] args) throws IOException, TimeoutException {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(HOST);
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-
-        System.out.println("Listening to queue " + QUEUE_NAME + " on host " + HOST);
-
-        for (int i = 0 ; i < 10 ; i++) {
-            sendMessage(channel, "Hello World " + i + "!");
+        if (args.length < 1) {
+            System.err.println("Usage: java -jar <app-jar> userId");
+            System.exit(1);
         }
 
-        channel.close();
-        connection.close();
+        String producerId = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+        String userId = args[0];
+
+        Connection connection = null;
+        Channel channel = null;
+
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost(HOST);
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+
+            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+            System.out.println(String.format("Sending messages to %s@%s/%s. To exit press CTRL+C", userId, EXCHANGE_NAME, HOST));
+
+            for (int i = 0; ; i++) {
+                sendMessage(channel, userId, String.format("Hello World %d from %s!", i, producerId));
+            }
+        } catch (RuntimeException e) {
+            if (channel != null) {
+                channel.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 
-    private static void sendMessage(Channel channel, String message) throws IOException {
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-
-        System.out.println("Sent: '" + message + "'");
+    private static void sendMessage(Channel channel, String userId, String message) throws IOException {
+        channel.basicPublish(EXCHANGE_NAME, userId, null, message.getBytes());
+        System.out.println(String.format("Sent to %s@%s/%s: %s", userId, EXCHANGE_NAME, HOST, message));
         sleepSec();
     }
 
@@ -44,5 +62,4 @@ public class Producer {
             throw new RuntimeException(e);
         }
     }
-
 }
