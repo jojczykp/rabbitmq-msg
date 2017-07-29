a1423e7c28713389bed9aea6449111757993e65d
jojczykp <pawel.jojczyk@gmail.com>
2017-07-29 23:49:57 +0100

    change client

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -2,43 +2,57 @@
 
 'use strict;'
 
-var amqp = require('amqplib/callback_api');
+var amqp = require('amqp'); //https://github.com/postwait/node-amqp
 
-var host = 'localhost';
-var exchangeName = 'sample-exchange';
 
 var args = process.argv.slice(2);
-if (args.length != 1) {
-    console.log('Usage: consumer.js consumerId');
+if (args.length != 2) {
+    console.log('Usage: consumer.js userId instanceId');
     process.exit(1);
 }
-var consumerId = args[0];
-var clientInstanceId = getRandomInt(100, 999);
 
-var authTokenTimestamp = 12345;
-var authTokenData = consumerId + ',' + authTokenTimestamp;
-var authTokenChecksum = 123;
-var authToken = 'Bearer ' + authTokenData + ',' + authTokenChecksum;
+var host = 'localhost';
+var port = 5672;
+var exchangeName = 'sample-exchange';
+var userId = args[0];
+var instanceId = args[1];
 
-// https://www.rabbitmq.com/uri-spec.html
-// authToken as user name, no password (same way as in 'immature' OAuth2 plugin)
-amqp.connect('amqp://' + authToken + '@' + host, function(err, conn) {
-    conn.createChannel(function(err, ch) {
-        var queueName = consumerId + '.' + clientInstanceId;
-        var queueProps = null; // Props come from policy defined on server
-        ch.assertQueue(queueName, queueProps, function(err, q) {
-            console.log('%s.%s: Waiting for messages to %s@%s/%s. To exit press CTRL+C', consumerId, clientInstanceId, consumerId, exchangeName, host);
-            ch.bindQueue(q.queue, exchangeName, consumerId);
-            ch.consume(q.queue, function(msg) {
-                console.log('%s.%s: Received on %s@%s/%s: %s', consumerId, clientInstanceId, msg.fields.routingKey, msg.fields.exchange, host, msg.content.toString());
-                ch.ack(msg);
-            }, {noAck: false});
+var timestampPeriodMillis = 15 * 60 * 1000;
+
+var connection = amqp.createConnection(
+{
+    host: host,
+    port: port,
+    login: instanceId + ',' + getAuthToken(userId), // TODO update on reconnect
+    password: ''
+}, {
+    reconnect: true
+});
+
+
+connection.on('ready', function () {
+    var queueName = userId + '.' + instanceId;
+    connection.queue(queueName, { exclusive: false, autoDelete: false },  function (q) {
+        console.log('%s.%s: Waiting for messages to %s@%s/%s. To exit press CTRL+C', userId, instanceId, userId, exchangeName, host);
+        q.bind(exchangeName, userId, function (q) {
+            console.log('%s.%s: Connection to %s@%s established', userId, instanceId, q.name, host);
+        });
+        q.subscribe(function (msg) {
+            console.log('%s.%s: Received on %s@%s/%s: %s', userId, instanceId, userId, exchangeName, host, msg.data.toString());
         });
     });
 });
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+function getAuthToken() {
+    var authTokenExpiryTimestamp = Date.now() + timestampPeriodMillis;
+    var authTokenData = userId + ',' + authTokenExpiryTimestamp;
+    var authTokenChecksum = 123;
+
+    return base64Encode('Bearer ' + authTokenData + ',' + authTokenChecksum);
+}
+
+
+function base64Encode(data) { // fake :)
+    return '[' + data + ']';
 }
