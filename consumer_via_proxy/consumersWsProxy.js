f622bc325a92b19d252fc506c3e10c60c09b0a69
jojczykp <pawel.jojczyk@gmail.com>
2017-08-10 00:58:48 +0100

    consumersWsProxy - multiple consumers

---
+++ consumer_via_proxy/consumersWsProxy.js
@@ -0,0 +1,105 @@
+'use strict;'
+
+
+const WebSocket = require('ws');
+
+
+var args = process.argv.slice(2);
+if (args.length < 2) {
+    console.log('Usage: %s initialConsumerId finalConsumerId [numberOfInstancesPerConsumer]', basename(process.argv[1]));
+    console.log('       Optional parameters default to 1');
+    process.exit(1);
+}
+
+
+const url = 'ws://localhost:9000';
+const initialConsumerId = parseInt(args[0]);
+const finalConsumerId = parseInt(args[1]);
+const numberOfInstancesPerConsumer = parseInt(args[2] || 1);
+
+const reconnectPeriod = 1000; // ms
+
+const authTokenPeriodMillis = 5 * 60 * 1000;
+
+
+console.log('Connecting to ' + url);
+console.log('To exit press CTRL+C');
+console.log('- initialConsumerId: ' + initialConsumerId);
+console.log('- finalConsumerId:   ' + finalConsumerId);
+console.log('- numberOfConsumers: ' + (finalConsumerId - initialConsumerId + 1));
+console.log('- numberOfInstancesPerConsumer: ' + numberOfInstancesPerConsumer);
+console.log('');
+
+
+var received = {};
+
+var numberOfInstances = 0;
+for (var consumerId = initialConsumerId ; consumerId <= finalConsumerId ; consumerId++) {
+    for (var instanceId = 1 ; instanceId <= numberOfInstancesPerConsumer ; instanceId++) {
+        startInstance(consumerId, instanceId);
+        numberOfInstances++;
+    }
+}
+
+
+function startInstance(consumerId, instanceId) {
+    const logPrefix = consumerId + '.' + instanceId + ': ';
+
+    const ws = new WebSocket(url);
+
+    ws.on('open', function open() {
+        ws.send(getUserDataStr('consumer' + consumerId, instanceId));
+        console.log(logPrefix + 'Subscribed');
+    });
+
+    ws.on('message', function incoming(data) {
+        var messageStr = data;
+        var oldCounter = received[messageStr] || 0;
+        var newCounter = oldCounter + 1;
+        received[messageStr] = newCounter;
+
+        if (newCounter == numberOfInstances) {
+            delete received[messageStr];
+            console.log('Received for (consumer%d to consumer%d)*%d: %d*[%s]',
+                   initialConsumerId, finalConsumerId, numberOfInstancesPerConsumer, numberOfInstances, messageStr);
+        }
+    });
+
+    ws.on('close', function incoming() {
+        console.log(logPrefix + 'Reconnecting with new Access Token');
+        setTimeout(() => startInstance(consumerId, instanceId), reconnectPeriod);
+    });
+
+    ws.on('error', function incoming(error) {
+        console.log(error.toString());
+    });
+}
+
+
+function getUserDataStr(consumerId, instanceId) {
+    return instanceId + ',' + getAuthToken(consumerId);
+}
+
+
+function getAuthToken(consumerId) {
+    var authTokenExpiryTimestamp = Date.now() + authTokenPeriodMillis;
+    var authTokenData = consumerId + ',' + authTokenExpiryTimestamp;
+    var authTokenChecksum = checksum(authTokenData);
+
+    return base64Encode('Bearer ' + authTokenData + ',' + authTokenChecksum);
+}
+
+
+function checksum(data) { // fake :)
+    return data.length;
+}
+
+
+function base64Encode(data) { // fake :)
+    return '[' + data + ']';
+}
+
+
+function basename(path) {
+    return path.split(/[\\/]/).pop();
+}
